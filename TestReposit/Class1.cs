59c71fe6c893a25645a3b4cb61338543297b09c1
caseyTW-CS <CT855@live.mdx.ac.uk>
2026-02-25 12:47:34 +0000

    Added classes: User, Train, Station

--- TestReposit/Class1.cs
+++ TestReposit/Class1.cs
@@ -10,4 +10,69 @@ namespace TestReposit
     {
         
     }
+
+    public class User
+    {
+        //PROPERTIES
+        public string userName {  get; private set; }
+        public string userPass { get; private set; }
+        //can be used for recommending tickets types/railcards:
+        public int userAge { get; private set; }
+        public string userEmail { get; private set;  }
+        //user phone number:
+        public string userPhone { get; private set; }
+        //for storing 
+        public List<string> userTickets { get; private set; }
+        public string userRailcard { get; private set; }
+
+        //METHODS
+        public User(string name, string pass, int age, string email, string phone, string railcard)
+        {
+            userName = name;
+            userPass = pass;
+            userAge = age;
+            userEmail = email;
+            userPhone = phone;
+            userTickets = new List<string>();
+        }
+    }
+    public class Train
+    {
+        //PROPERTIES
+        public string trainNumber {  get; private set; }
+        //referring to commuter, freight, etc:
+        public string trainType { get; private set; }
+        //e.g. thameslink:
+        public string trainCompany { get; private set; }
+        public int trainCarriages {  get; private set; }
+        //will be used to store the names of each stop on the journey in a list:
+        public List<string> trainStops { get; private set; }
+
+        //METHODS
+        public Train(string number, string type, string company, int carriages)
+        {
+            trainNumber = number;
+            trainType = type;
+            trainCompany = company;
+            trainCarriages = carriages;
+            trainStops = new List<string>();
+        }
+    }
+    public class Station
+    {
+        //PROPERTIES
+        public string stationName { get; private set;  }
+        //for storing the size of the platforms:
+        public string stationSize { get; private set; }
+        //for the platform the train will be stopping on:
+        public int stationPlatform { get; private set; }
+        
+        //METHODS
+        public Station (string name, string size, int platform)
+        {
+            stationName = name;
+            stationSize = size;
+            stationPlatform = platform;
+        }
+    }
 }
